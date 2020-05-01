2ff201e98aeeb3e5c2e71a28b398b48590da6c91
Cristiano <cristiano_santos93@hotmail.com>
2020-05-01 12:21:38 +0100

    javascript1

--- WebTech_Assessment/validation.js
+++ WebTech_Assessment/validation.js
@@ -1,41 +1,71 @@
-
-
-
-function main() {
-    document.getElementById('suggestion_form').addEventListener('submitButton', handleSubmit)
-}
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("suggestion_form").addEventListener("submit", handleSubmit)
+});
 
 function handleSubmit(event) {
-    
-    var myForm = document.getElementById('suggestion_form');
-    var valid = true;
-
-    if (myForm.email.value == '') {
-        event.preventDefault();  
-        valid = false;
-        document.getElementById('emailRequiredError').style.display = 'block';
-    } else {
-        document.getElementById('emailRequiredError').style.display = 'none';
-        valid = true;
-    }
-    
-
-
-
-    if(myForm.name.value == '') {
-        event.preventDefault();  
-        valid = false;
-        document.getElementById('nameRequiredError').style.display = 'block';
-    } else {
-        document.getElementById('nameRequiredError').style.display = 'none';
-        valid = true;
-    }
-    
-
-
-    if(valid) {
-        console.log('form submitted')
-    }
-    
     event.preventDefault();         // this is so the form is not sent  
+    console.log(event.target);
+    var myForm = document.getElementById("suggestion_form");
+    var valid = true;
+    console.log(myForm.email.value +' '+ myForm.name.value);
+    var children = ["email","name","theme"];
+
+    for (var i = children.length - 1; i >= 0; i--) {
+        if (myForm[children[i]].value == ""){
+            if (valid) {
+                valid = false;
+            }
+            document.getElementById(children[i] + "RequiredError").style.display = "block";
+        }
+    }
+    if(valid) {
+        console.log("form submitted")
+    }
 }
+
+
+
+
+
+// function main() {
+//     var myForm = document.getElementById('suggestion_form');
+//     myForm.addEventListener('submit',handleSubmit);    
+
+// }
+// function handleSubmit(event) {
+//     event.preventDefault();
+//     var myForm = document.getElementById("suggestion_form");
+//     var valid = true;
+//     var isEmpty =true;
+    
+
+//     if (myForm.email.value == "") {
+         
+//         valid = false;
+//         document.getElementById('emailRequiredError').style.display = 'block';
+//         event.preventDefault();
+//     } else {
+//         document.getElementById('emailRequiredError').style.display = 'none';        
+//     }
+    
+
+
+
+//     if(myForm.name.value == '') {
+        
+//         isEmpty = false;
+//         document.getElementById('nameRequiredError').style.display = 'block';
+//         event.preventDefault();
+//     } else {
+//         document.getElementById('nameRequiredError').style.display = 'none';        
+//     }
+    
+
+
+//     if(valid) {
+//         console.log('name submitted');
+//     } 
+//     if (isEmpty) {
+//         console.log('email submited');
+//     }
+// }
